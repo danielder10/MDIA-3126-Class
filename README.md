f7d8d2f684413f52984b2e40b9c6aaa2d553d322
danielder10 <dder10@my.bcit.ca>
2024-10-08 13:34:08 -0700

    content updates for week 6

--- README.md
+++
@@ -1,2 +0,0 @@
-# MDIA-3126-Class
-this repo is for  MDIA-3126 from week 3 - End of semester
--- mobile-app/app/(tabs)/rest.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from 'react-native';
-
-export default function Home2() {
-  return <Text>This Is My Second Home Page</Text>;
-}
